9652f66a9e1e90d59c3dd14b8d4644264f82af20
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-08-17 23:32:48 +0900

    fix form

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const authClient = new google.auth.JWT(
 		const res = await service.spreadsheets.values.get({
 			auth: authClient,
 			spreadsheetId: "1r59N6wuaaW4Ue4heIyoXp3oafpLkz2lqVB5VTmsWPc0",
-			range: "A:B",
+			range: "A:E",
 		});
 
 		// All of the answers
@@ -43,7 +43,7 @@ const authClient = new google.auth.JWT(
 
 			// For each row
 			for (const row of rows) {
-					answers.push({ timeStamp: row[0], answer: row[1] });
+					answers.push({ timeStamp: row[0], answer1: row[1],  answer2: row[2], answer3: row[3], answer4: row[4]});
 			}
 
 		} else {
