38b54946f519c4d599036f348b7106a39e248033
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-08-18 23:54:15 +0900

    formed issue content

--- create_issue.js
+++ create_issue.js
@@ -4,19 +4,17 @@ const octokit = new Octokit({auth: process.env.ACCESS_TOKEN});
 const latestAnswer = require("./answers.json").slice(-1)[0]
 
 const parse = JSON.stringify(latestAnswer)
-const hoge = JSON.parse(parse)
 
-// console.log(hoge)
+console.log(latestAnswer)
 
 octokit.issues.create({
   owner: 'mm-masahiro',
   repo: 'google-form',
   title: process.argv[2],
-  body: latestAnswer,
+  // body: parse,
+  body: `${latestAnswer.answer1}\n${latestAnswer.answer2}\n${latestAnswer.answer3}\n${latestAnswer.answer4}`,
 }).then(({data}) => {
   console.log(data);
 }).catch(error => {
   console.log(error);
 });
-
-// console.log(parse)
