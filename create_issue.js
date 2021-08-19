d719434425f949aef58fa7481fc4ccd9c188011d
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-08-19 22:27:24 +0900

    fix body

--- create_issue.js
+++ create_issue.js
@@ -5,13 +5,11 @@ const latestAnswer = require("./answers.json").slice(-1)[0]
 
 const parse = JSON.stringify(latestAnswer)
 
-console.log(latestAnswer)
-
 octokit.issues.create({
   owner: 'mm-masahiro',
   repo: latestAnswer.repositoryName,
   title: process.argv[2],
-  body: `${latestAnswer.answer1}\n${latestAnswer.answer2}\n${latestAnswer.answer3}\n${latestAnswer.answer4}`,
+  body: `### 勉強会の雰囲気はどうでしたか？\n- ${latestAnswer.answer1}\n### 勉強会を開催してみての感想\n- ${latestAnswer.answer2}\n### 改善できそうなポイント（主催者側）\n- ${latestAnswer.answer3}\n### 参加者に「もう少しこうして欲しかった」というポイント\n- ${latestAnswer.answer4}`,
 }).then(({data}) => {
   console.log(data);
 }).catch(error => {
