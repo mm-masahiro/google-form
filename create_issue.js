848530be00f43df13fcec5d2bbb99f76e461ed71
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-09-25 15:35:28 +0900

    fix reoisitory auth

--- create_issue.js
+++ create_issue.js
@@ -4,7 +4,7 @@ const octokit = new Octokit({auth: process.env.ACCESS_TOKEN});
 const latestAnswer = require("./answers.json").slice(-1)[0]
 
 octokit.issues.create({
-  owner: 'mm-masahiro',
+  owner: 'tetsu-tech',
   repo: latestAnswer.repositoryName,
   title: process.argv[2],
   body: `### 勉強会の雰囲気はどうでしたか？\n- ${latestAnswer.answer1}\n### 勉強会を開催してみての感想\n- ${latestAnswer.answer2}\n### 改善できそうなポイント（主催者側）\n- ${latestAnswer.answer3}\n### 参加者に「もう少しこうして欲しかった」というポイント\n- ${latestAnswer.answer4}`,
